bd780078f5314f2f439c161571fabf364c9cad41
georgisoare <72292330+georgisoare@users.noreply.github.com>
2022-05-14 18:26:09 +0300

    final touch

--- app.js
+++ app.js
@@ -1,8 +1,90 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us8.api.mailchimp.com/3.0/lists/7d9bcd96d6";
+  const options = {
+    method: "POST",
+    auth: "georgi:c6691092ab61fbf3ed4983063521dd0b-us8",
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/homepage.html");
+    }
+
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/weather", function (req, res) {
+  const query = req.body.cityName;
+  const apiKey = "27183f435226ede8a5a5b4c36573de02";
+  const unit = "metric";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=" +
+    apiKey +
+    "&units=" +
+    unit;
+
+  https.get(url, function (response) {
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const description = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+      res.write(
+        "<h1>The temperature in " +
+          query +
+          " is " +
+          temp +
+          " degrees Celcius.</h1>"
+      );
+      res.write("<h2>The weather is currently: " + description + ".</h2>");
+      res.write("<img src= " + imageURL + "></img>");
+      res.send();
+    });
+  });
+});
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
